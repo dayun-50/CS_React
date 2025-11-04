3d69b59729625dbcb1ee69fdf7ba9d45164a7282
Fuigang <quickly3899@naver.com>
2025-11-04 10:48:35 +0900

    login merged

--- src/notMember/login/Login.jsx
+++ src/notMember/login/Login.jsx
@@ -39,34 +39,6 @@ function Login() {
               placeholder="이메일"
               value={id}
               onChange={hendleChangeById}
-            />
-          </div>
-
-          <div className={styles.pwbox}>
-            <label htmlFor="pw">비밀번호</label>
-            <br />
-            <input
-              id="pw"
-              type="password"
-              placeholder="비밀번호"
-              value={pw}
-              onChange={hendleChangeByPw}
-            />
-          </div>
-
-          <button className={styles.loginbutton} onClick={clickLogin}>
-            로그인
-          </button>
-
-          <div className={styles.emailbox}>
-            <label htmlFor="email">이메일</label>
-            <br />
-            <input
-              id="email"
-              type="email"
-              placeholder="이메일"
-              value={id}
-              onChange={hendleChangeById}
               onKeyDown={(e) => {
                 if (e.key === "Enter") clickLogin();
               }}
